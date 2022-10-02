c477a90057b5668c6382dc173d8d9c3f6f5249c8
Saad Shaikh <saadsk9288@gmail.com>
2022-10-02 13:29:31 +0530

    committing

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,18 +4,26 @@ import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar.js";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer.js";
 class Layout extends Component {
-   state={
-    showSideDrawer: true
-   }
-   
-    SideDrawerClosedHandler=()=>{
-this.setState({showSideDrawer: false})
-    }
+  state = {
+    showSideDrawer: true,
+  };
+
+  SideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
   render() {
     return (
       <Aux>
         <Toolbar />
-        <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.SideDrawerClosedHandler}
+        />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>{this.props.children}</main>
       </Aux>
