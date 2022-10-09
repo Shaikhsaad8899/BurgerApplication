4e105c9317d821f64d8e23feb53221d04a584a28
Saad Shaikh <saadsk9288@gmail.com>
2022-10-09 23:27:57 +0530

    Order Page added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const app = (
-    <Router>
+    <BrowserRouter>
         <App />
-    </Router>
+    </BrowserRouter>
 );
 
 ReactDOM.render( app, document.getElementById( 'root' ) );
